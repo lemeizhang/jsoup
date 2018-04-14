38c5a817bace6e011eac225d9c2e8d6fa53b7caf
LEMEI ZHANG <lemeiz@cs.washington.edu>
2018-04-13 18:20:06 -0700

    additional unit tests for TokenQueue.java

--- src/test/java/org/jsoup/parser/TokenQueueTest.java
+++ src/test/java/org/jsoup/parser/TokenQueueTest.java
@@ -4,6 +4,8 @@ import org.jsoup.Jsoup;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Token queue tests.
@@ -77,6 +79,62 @@ public class TokenQueueTest {
         validateNestedQuotes("<html><body><a id=\"identifier\" onclick=func('arg') /></body></html>", "a[onclick*=\"('arg\"]");
         validateNestedQuotes("<html><body><a id=\"identifier\" onclick='func(\"arg\")' /></body></html>", "a[onclick*='(\"arg']");
         validateNestedQuotes("<html><body><a id=\"identifier\" onclick=func(\"arg\") /></body></html>", "a[onclick*='(\"arg']");
+    }
+
+
+    @Test public void testPeek() {
+        TokenQueue tq1 = new TokenQueue("abc");
+        TokenQueue tq2 = new TokenQueue("");
+
+        assertEquals('a', tq1.peek());
+        assertEquals(0, tq2.peek());
+    }
+
+    @Test public void testMatchesCS() {
+        TokenQueue tq = new TokenQueue("Hello");
+        assertTrue(tq.matchesCS("Hello"));
+        assertFalse(tq.matchesCS("hello"));
+    }
+
+
+    @Test public void testMatchesStartTag() {
+        TokenQueue tq1 = new TokenQueue("<p>");
+        TokenQueue tq2 = new TokenQueue("abc");
+        TokenQueue tq3 = new TokenQueue("<");
+        TokenQueue tq4 = new TokenQueue("<<");
+
+        assertTrue(tq1.matchesStartTag());
+        assertFalse(tq2.matchesStartTag());
+        assertFalse(tq3.matchesStartTag());
+        assertFalse(tq4.matchesStartTag());
+    }
+
+    @Test public void testConsumeTagName() {
+        TokenQueue tq1 = new TokenQueue("p");
+        TokenQueue tq2 = new TokenQueue("tag_name");
+        TokenQueue tq3 = new TokenQueue("tag-name");
+        TokenQueue tq4 = new TokenQueue("ab:cd");
+        TokenQueue tq5 = new TokenQueue("%%%");
+
+        assertEquals("p", tq1.consumeTagName());
+        assertEquals("tag_name", tq2.consumeTagName());
+        assertEquals("tag-name", tq3.consumeTagName());
+        assertEquals("ab:cd", tq4.consumeTagName());
+        assertEquals("", tq5.consumeTagName());
+    }
+
+    @Test public void testConsumeAttribtueKey() {
+        TokenQueue tq1 = new TokenQueue("href");
+        TokenQueue tq2 = new TokenQueue("_position");
+        TokenQueue tq3 = new TokenQueue("http-equiv");
+        TokenQueue tq4 = new TokenQueue("xml:lang");
+        TokenQueue tq5 = new TokenQueue("%%%");
+
+        assertEquals("href", tq1.consumeAttributeKey());
+        assertEquals("_position", tq2.consumeAttributeKey());
+        assertEquals("http-equiv", tq3.consumeAttributeKey());
+        assertEquals("xml:lang", tq4.consumeAttributeKey());
+        assertEquals("", tq5.consumeAttributeKey());
     }
 
     private static void validateNestedQuotes(String html, String selector) {
